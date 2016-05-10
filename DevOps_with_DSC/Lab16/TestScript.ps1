1a2da182e1f149348dc77d05abf31e6573089cfc
Peter Ericsson <peter.ericsson@addlevel.se>
2016-05-10 14:26:14 +0200

    Signed-off-by: Peter Ericsson <peter.ericsson@addlevel.se>

---
+++ DevOps_with_DSC/Lab16/TestScript.ps1
@@ -0,0 +1,5 @@
+﻿#Install-Module cChoco             -RequiredVersion 2.0.5.22
+#Install-Module xWebAdministration -RequiredVersion 1.10.0.0
+Import-DscResource -ModuleName cChoco
+Import-DscResource -ModuleName xWebAdministration
+Import-DscResource –ModuleName PSDesiredStateConfiguration
