836e370321e5e170e191d6a2ed5d2f093700b874
lgrstrm <jonas@lgrstrm.io>
2016-05-19 10:43:53 +0200

    Changed variables to use configdata

--- DevOps_with_DSC/Lab07/SetCA.ps1
+++ DevOps_with_DSC/Lab07/SetCA.ps1
@@ -1,16 +1,16 @@
                 #region configure CA settings and prepare AIA / CDP
                 New-Item c:\CDP -ItemType directory -Force
-                Copy-Item C:\Windows\System32\CertSrv\CertEnroll\*.crt C:\CDP\$using:CAName.crt -Force
+                Copy-Item C:\Windows\System32\CertSrv\CertEnroll\*.crt C:\CDP\$($using:Node.CAName).crt -Force
                 Get-CAAuthorityInformationAccess | Remove-CAAuthorityInformationAccess -Force
                 Get-CACrlDistributionPoint | Remove-CACrlDistributionPoint -Force
-                Add-CAAuthorityInformationAccess -Uri http://$using:CDPURL/$using:CAName.crt -AddToCertificateAia -Force
+                Add-CAAuthorityInformationAccess -Uri http://$($using:Node.CDPURL)/$($using:Node.CAName).crt -AddToCertificateAia -Force
                 Add-CACrlDistributionPoint -Uri C:\CDP\<CAName><CRLNameSuffix><DeltaCRLAllowed>.crl -PublishToServer -PublishDeltaToServer -Force
-                Add-CACrlDistributionPoint -Uri http://$using:CDPURL/<CAName><CRLNameSuffix><DeltaCRLAllowed>.crl -AddToCertificateCdp -AddToFreshestCrl -Force
+                Add-CACrlDistributionPoint -Uri http://$($using:Node.CDPURL)/<CAName><CRLNameSuffix><DeltaCRLAllowed>.crl -AddToCertificateCdp -AddToFreshestCrl -Force
                 #endregion configure CA settings and prepare AIA / CDP
  
                 #region create CDP / AIA web site
                 Import-Module 'C:\Windows\system32\WindowsPowerShell\v1.0\Modules\WebAdministration\WebAdministration.psd1'
-                New-Website -Name CDP -HostHeader $using:CDPURL -Port 80 -IPAddress * -Force
+                New-Website -Name CDP -HostHeader $($using:Node.CDPURL) -Port 80 -IPAddress * -Force
                 Set-ItemProperty 'IIS:\Sites\CDP' -Name physicalpath -Value C:\CDP
                 Set-WebConfigurationProperty -PSPath 'IIS:\Sites\CDP' -Filter /system.webServer/directoryBrowse  -Name enabled -Value true
                 Set-WebConfigurationProperty -PSPath 'IIS:\Sites\CDP' -Filter /system.webServer/security/requestfiltering  -Name allowDoubleEscaping -Value true
